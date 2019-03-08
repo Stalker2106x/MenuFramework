89882bfdbb9c1da2f93a17e4b615567bffca1d9b
Maxime MARTENS KLEE <maxime.martens-ext@kleegroup.com>
2019-03-08 11:09:01 +0100

    added single header include

---
+++ include/MenuFramework.hpp
@@ -0,0 +1,13 @@
+#ifndef MENUFRAMEWORK_HPP_
+#define MENUFRAMEWORK_HPP_
+
+#include "DataFile.hh"
+#include "GraphicsRenderer.hh"
+#include "InputManager.hpp"
+#include "Localization.hh"
+#include "Menu.hh"
+#include "MenuItem.hh"
+#include "Point.hpp"
+#include "ScriptEngine.hh"
+
+#endif /* !MENUFRAMEWORK_HPP_ */
