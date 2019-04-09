3d3eae876a2821972e9e683547dd3df48513676f
Maxime MARTENS <contact@maximemartens.fr>
2019-04-10 00:07:14 +0200

    Updated tests for more coverage in CI

--- test/basic.cpp
+++ test/basic.cpp
@@ -1,21 +1,31 @@
 #include "MenuFramework.hpp"
 #include <string>
-#include <thread>
-#include <chrono>
 
 int main(int argc, char **argv)
 {
-	std::string menuDocument;
-
-	menuDocument += "<Menu OnLoad='InitScript'>"
-		" <Text>Menu Example</Text>"
-		" <Sep/>"
-		" <Button Type='Intern' Target='exit()'>ExitTest</Button>"
-		" <Button Type='Intern' Target='exit()'>SS</Button>"
-		"</Menu>"
-		"<Script Id='InitScript'><![CDATA["
-		"exit()"
-		"]]></Script>";
+	std::string menuDocument = "<Menu OnLoad='InitScript'>"
+														 " <Text>Menu Example</Text>"
+														 " <Sep/>"
+														 " <Button Type='Intern' Target=''>ButtonTest</Button>"
+  													 " <Select Id='SelectTest'>"
+  													 "  <Option Value='1'>Option1</Option>"
+  													 "  <Option Value='2'>Option2</Option>"
+  													 " </Select>"
+  													 " <Input Id='InputTest' />"
+  													 " <Script><![CDATA["
+														 "	 alert('Hello, i'm an alert')"
+														 "	 tostring(5)"
+														 "	 getCursor()"
+														 "	 getInputData(\"InputTest\")"
+														 "	 setInputData(\"InputTest\", \"This is a test\")"
+														 "	 getSelectValue(\"SelectTest\")"
+														 "	 setSelectValue(\"SelectTest\", \"2\")"
+														 "  addMenuItem(0, \"<Text>This is the new title</Text>\")"
+														 " ]]</Script>"
+														 "</Menu>"
+														 "<Script Id='InitScript'><![CDATA["
+														 "exit()"
+														 "]]></Script>";
   Menu::goTo("", menuDocument, DataSource::Document);
   Menu::run();
   Menu::unload();
